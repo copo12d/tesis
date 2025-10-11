813814caf69037cdcc8a57733bcb4989d82b3b17
Jose <jvilchezarteaga@gmail.com>
2025-10-11 12:25:55 -0400

    merge

--- backend/src/main/java/com/tesisUrbe/backend/solidWasteManagement/dto/BatchRegResponseDto.java
+++ backend/src/main/java/com/tesisUrbe/backend/solidWasteManagement/dto/BatchRegResponseDto.java
@@ -3,21 +3,13 @@ package com.tesisUrbe.backend.solidWasteManagement.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
-<<<<<<<< HEAD:backend/src/main/java/com/tesisUrbe/backend/solidWasteManagement/dto/WasteResponseDto.java
-@AllArgsConstructor
-public class WasteResponseDto {
-    private Long id;
-    private BigDecimal weight;
-    private LocalDateTime collectionDate;
-    private Long containerId;
-    private Long batchId;
-========
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -27,5 +19,4 @@ public class BatchRegResponseDto {
     private String createdByUsername;
     private String date;
     private String hour;
->>>>>>>> origin/angel-branch:backend/src/main/java/com/tesisUrbe/backend/solidWasteManagement/dto/BatchRegResponseDto.java
 }
