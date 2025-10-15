6ed8009f7fa12c4b6f65d7b7fec4d9024f473da3
luisjuarezve <163745370+luisjuarezve@users.noreply.github.com>
2025-10-14 20:23:05 -0400

    add env

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { refreshToken } from '../service/RefreshToken';
 
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: `${BASE_URL}`,
