476d7038ed25b5b8860311b9aee6b01a50c51f37
luisjuarezve <163745370+luisjuarezve@users.noreply.github.com>
2025-10-29 22:55:34 -0400

    FullContainerSummary

--- frontend/src/features/dashboard/api/dashboard.api.js
+++ frontend/src/features/dashboard/api/dashboard.api.js
@@ -4,4 +4,5 @@ export const DashboardAPI = {
   getWeekly: () => api.get('/dashboard/weekly'),
   getCards: () => api.get('/dashboard/cards'),
   getActiveContainerSummary: () => api.get('/container/public/active-summary'),
+  ContainerTypeSummary: () => api.get('/container/public/full-summary'),
 };
