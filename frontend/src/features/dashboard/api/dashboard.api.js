1b2a015b35e2169ec93d2c172c9ca492312e1162
luisjuarezve <163745370+luisjuarezve@users.noreply.github.com>
2025-11-01 17:14:22 -0400

    update dashboard add endpoint

--- frontend/src/features/dashboard/api/dashboard.api.js
+++ frontend/src/features/dashboard/api/dashboard.api.js
@@ -6,4 +6,5 @@ export const DashboardAPI = {
   getActiveContainerSummary: () => api.get('/container/public/active-summary'),
   ContainerTypeSummary: () => api.get('/container/public/full-summary'),
   ProcessedBatchSummary: () => api.get('/batch/public/processed-summary'),
+  getDailyContainerSummary: () => api.get('/batch-reg/public/daily-summary'),
 };
